a44bb3db7a882f2c359a49064bf4c6d188395887
Haiyang Hong <hong961127@gmail.com>
2020-08-17 21:06:50 -0700

    Build Bidirectional Relations

--- schema/schema.js
+++ schema/schema.js
@@ -3,36 +3,45 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
 const axios = require("axios");
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      async resolve(parentVal, args) {
+        const res = await axios.get(
+          `http://localhost:3000/companies/${parentVal.id}/users`
+        );
+        return res.data;
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
     company: {
       type: CompanyType,
       async resolve(parentVal, args) {
-        console.log(parentVal);
         const res = await axios.get(
           `http://localhost:3000/companies/${parentVal.companyId}`
         );
         return res.data;
       },
     },
-  },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -44,6 +53,20 @@ const RootQuery = new GraphQLObjectType({
       async resolve(parentVal, args) {
         try {
           const res = await axios.get(`http://localhost:3000/users/${args.id}`);
+          return res.data;
+        } catch (error) {
+          console.error(error);
+        }
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parentVal, args) {
+        try {
+          const res = await axios.get(
+            `http://localhost:3000/companies/${args.id}`
+          );
           return res.data;
         } catch (error) {
           console.error(error);
